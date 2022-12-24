1a06e4a36cfce5bd0ae71146fded987f922ced89
KotaroKazama0624 <ec27096h@gmail.com>
2022-12-24 18:10:52 +0900

    secret_keyの修正

--- jira_api/jira_api/settings_local.py
+++ jira_api/jira_api/settings_local.py
@@ -1,2 +1,3 @@
 #SECRET_KEY = 'django-insecure-z4%f2w$z0v)38^-h3jr_go^d4_qn6$dxd7@$aqsn9zfa*mqdsw'
-SECRET_KEY = '*#iq@^8hd1e+su&blq4%&7x+q2pz9lekw__uu5dm=fjy=laf_5'
+#SECRET_KEY = '*#iq@^8hd1e+su&blq4%&7x+q2pz9lekw__uu5dm=fjy=laf_5'
+SECRET_KEY = 'p462k#q=w7#%)!_)9m0*6^q!^lco690w(5a#str@349c2=ri!n'
